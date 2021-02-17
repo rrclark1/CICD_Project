9d41a5fb3ce1a07f6d2559f609f549e607008370
rrclark1 <43519018+rrclark1@users.noreply.github.com>
2021-02-17 04:49:41 +0000

    Update Project1Application.java

--- src/main/java/com/project/project1/Project1Application.java
+++ src/main/java/com/project/project1/Project1Application.java
@@ -3,7 +3,7 @@ package com.project.project1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication;
+@SpringBootApplication
 public class Project1Application {
 
 	public static void main(String[] args) {
